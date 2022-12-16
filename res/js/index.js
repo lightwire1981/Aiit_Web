b47fb3232371771613d98cd20c84ef2d20a8e52b
Lee mintae <mintael@gmail.com>
2022-12-16 16:03:30 +0900

    시리얼 전송 기능 업데이트

--- res/js/index.js
+++ res/js/index.js
@@ -1,12 +1,32 @@
+let port_num;
+
 $(document).ready(function () {
-    serialTest();
+    $("#btnGetSerial").on('click', serialTest);
+    $("#btnSendMsg").on('click', writeAiit);
 });
-function serialTest() {
+async function serialTest() {
+
     if ("serial" in navigator) {
-        alert("시리얼 OK");
         console.log("시리얼 OK");
+        await navigator.serial.requestPort().then(async (port) => {
+            console.log(port);
+            port_num = port;
+            await port_num.open({baudRate: 9600});
+        }).catch((e) => {
+            console.log(e);
+        });
     } else {
         alert("시리얼 Fail");
         console.log("시리얼 지원 안됨");
     }
+}
+
+async function writeAiit() {
+    const textEncoder = new TextEncoderStream();
+    // const writableStreamClosed = textEncoder.readable.pipeTo(port_num.writable);
+    await textEncoder.readable.pipeTo(port_num.writable);
+
+    const writer = textEncoder.writable.getWriter();
+
+    await writer.write("QAT01");
 }
