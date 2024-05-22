f33563d0543ff5208288392c137c7836266d81a0
lightwire1981 <mintael@gmail.com>
2024-05-22 15:59:51 +0900

    predict file update

--- res/js/index.js
+++ res/js/index.js
@@ -1,99 +1,61 @@
-let port;
-let textEncoder;
-let writableStreamClosed;
-let writer;
+const DEBUG_DISABLE = false;
 
 $(document).ready(function () {
-    $("#btnGetSerial").on('click', serialTest);
-    $("#btnSendMsg").on('click', writeAiit);
-    $("#btnModelLoad").on('click', init);
 });
-async function serialTest() {
 
-    if ("serial" in navigator) {
-        console.log("USB Serial OK");
-        await navigator.serial.requestPort().then(async (result) => {
 
-            port = result;
-            await port.open({baudRate: 9600}).then(()=>{
-                readyToWrite();
-            });
-            console.log(port);
-        }).catch((e) => {
-            console.log(e);
-        });
-    } else {
-        alert("USB Serial 통신 실패");
-        console.log("USB Serial Failed");
+
+let menuOpen = false;
+$(document).click(function (e) {
+    if (menuOpen) {
+        if (!$(e.target).hasClass('SpnMenu')) {
+            let menuIcon = $('#menu_icon');
+            menuIcon.prop('checked', false);
+            menuIcon.change();
+        }
     }
+});
+
+window.onload = function () {
+    $('#menu_icon').change(menuControl);
+    setWidget();
 }
 
-function readyToWrite() {
-    textEncoder = new TextEncoderStream();
-    writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
-    writer = textEncoder.writable.getWriter();
+function setWidget() {
+    $('#btnMainAiitkit').on('click', loadPage);
+
+    $('#divMainContents').load('page/aiit.html');
 }
 
-async function writeAiit() {
-
-    await writer.write("QAT01");
+function menuControl() {
+    menuOpen = $(this).prop('checked');
+    debugMessage('Menu Open', menuOpen);
 }
 
-// More API functions here:
-// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+function loadPage() {
+    debugMessage('Page ID', this.id);
+    let mainPage = $('#divMainContents');
 
-// the link to your model provided by Teachable Machine export panel
-const URL = "https://teachablemachine.withgoogle.com/models/J-gOsDHsq/";
-// const URL = "./my_model/";
 
-let model, webcam, labelContainer, maxPredictions;
-let modelURL, metadataURL
+}
 
-// Load the image model and setup the webcam
-async function init() {
-    let txtUrl = $("#txtModelUrl");
-    if (txtUrl.val().length > 0) {
-        let user_url = txtUrl.val();
-        modelURL = user_url + "model.json";
-        metadataURL = user_url + "metadata.json";
-    } else {
-        modelURL = URL + "model.json";
-        metadataURL = URL + "metadata.json";
+
+
+
+
+
+/**
+ * 디버그 출력 포맷
+ * @param index 안내
+ * @param message 메시지
+ */
+function debugMessage(index, message) {
+    if (DEBUG_DISABLE) {
+        return;
     }
-
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // or files from your local hard drive
-    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-    model = await tmImage.load(modelURL, metadataURL);
-    maxPredictions = model.getTotalClasses();
-
-    // Convenience function to setup a webcam
-    const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
-    await webcam.play();
-    window.requestAnimationFrame(loop);
-
-    // append elements to the DOM
-    document.getElementById("webcam-container").appendChild(webcam.canvas);
-    labelContainer = document.getElementById("label-container");
-    for (let i = 0; i < maxPredictions; i++) { // and class labels
-        labelContainer.appendChild(document.createElement("div"));
+    if (message==null) {
+        window.console.log("DEBUG[index.js] : %s",index);
+        return;
     }
-}
-
-async function loop() {
-    webcam.update(); // update the webcam frame
-    await predict();
-    window.requestAnimationFrame(loop);
-}
-
-// run the webcam image through the image model
-async function predict() {
-    // predict can take in an image, video or canvas html element
-    const prediction = await model.predict(webcam.canvas);
-    for (let i = 0; i < maxPredictions; i++) {
-        labelContainer.childNodes[i].innerHTML = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-    }
+    window.console.log("DEBUG[index.js]-%s : %o",index,message);
 }
--- res/js/predict.js
+++ res/js/predict.js
@@ -1,24 +1,24 @@
 // run the webcam image through the image model
 async function predict() {
-	// predict can take in an image, video or canvas html element
-	let image = document.getElementById("face-image")
-	// const prediction = await model.predict(webcam.canvas);
-	const prediction = await model.predict(image, false);
-	testFunc();
-	for (let i = 0; i < maxPredictions; i++) {
-		const classPrediction =
-			prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
-		// labelContainer.childNodes[i].innerHTML = classPrediction;
-		let cname = document.createElement('div');
-		cname.setAttribute("class", "cname");
-		cname.innerHTML = prediction[i].className;
-		let graph = document.createElement('div');
-		graph.setAttribute("class", "graph");
-		let result = document.createElement('span');
-		result.style.width = Math.round(prediction[i].probability.toFixed(2)*100)+"%";
-		result.innerHTML = prediction[i].probability.toFixed(2)*100+"%";
-		graph.appendChild(result);
-		labelContainer.childNodes[i].appendChild(cname);
-		labelContainer.childNodes[i].appendChild(graph);
-	}
+	// // predict can take in an image, video or canvas html element
+	// let image = document.getElementById("face-image")
+	// // const prediction = await model.predict(webcam.canvas);
+	// const prediction = await model.predict(image, false);
+	// testFunc();
+	// for (let i = 0; i < maxPredictions; i++) {
+	// 	const classPrediction =
+	// 		prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+	// 	// labelContainer.childNodes[i].innerHTML = classPrediction;
+	// 	let cname = document.createElement('div');
+	// 	cname.setAttribute("class", "cname");
+	// 	cname.innerHTML = prediction[i].className;
+	// 	let graph = document.createElement('div');
+	// 	graph.setAttribute("class", "graph");
+	// 	let result = document.createElement('span');
+	// 	result.style.width = Math.round(prediction[i].probability.toFixed(2)*100)+"%";
+	// 	result.innerHTML = prediction[i].probability.toFixed(2)*100+"%";
+	// 	graph.appendChild(result);
+	// 	labelContainer.childNodes[i].appendChild(cname);
+	// 	labelContainer.childNodes[i].appendChild(graph);
+	// }
 }
